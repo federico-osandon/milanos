f70fe5a6989fe33a61392c6ba07486b864c53bae
FedericoOsandon <51425658+FedericoOsandon@users.noreply.github.com>
2019-12-12 13:26:36 -0300

    Update settings.py

--- milanos/settings.py
+++ milanos/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'milanos.urls'
@@ -125,6 +126,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = reverse_lazy('listar_venta')
 LOGOUT_REDIRECT_URL = reverse_lazy('login')
